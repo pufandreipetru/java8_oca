95e3f02441c598ed93d9c26bd40d9eca3a3b1190
Andrei Puf <andrei.puf@Andreis-MacBook-Pro.local>
2020-02-07 12:29:14 +0200

    capter1 - Identifiers and key words

---
+++ src/main/java/capter1/IdentifiersAndKeyWords.java
@@ -0,0 +1,25 @@
+package capter1;
+
+public class IdentifiersAndKeyWords {
+
+    public static void main(String[] args) {
+        /**
+         * Legal identifiers
+         */
+        int _a;
+        int $c;
+        int _______2_w;
+        int _$;
+        int this_is_a_very_detailed_name_for_an_identifier;
+
+        /**
+         * Illegal identifiers
+         */
+
+        int :b;
+        int -d:
+        int e#;
+        int .f;
+        int 7g;
+    }
+}
---
+++ src/main/java/capter1/README.md
@@ -0,0 +1,5 @@
+Declaration and access control
+-
+* IdentifiersAndKeyWords.java
+    * Legal identifiers
+    * Illegal identifiers
