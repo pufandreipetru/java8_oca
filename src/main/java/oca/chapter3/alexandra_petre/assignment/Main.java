18a902e884576c3062fac6354f5753cb3eadecd3
Alexandra Petre <v-alpetre@expediagroup.com>
2020-03-12 10:15:59 +0200

    chapter3 - assignments

---
+++ src/main/java/oca/chapter3/alexandra_petre/assignment/Main.java
@@ -0,0 +1,25 @@
+package oca.chapter3.alexandra_petre.assignment;
+
+public class Main {
+    public static void main(String[] args) {
+        int y = 10;
+        int x = 20;
+
+        y -= 6;
+        System.out.println("y = " + y);
+
+        x += 4 * 10;
+        System.out.println("x = " + x);
+
+        int a = 23;
+        a %= 2;
+        System.out.println("a = " + a);
+
+//        a = -a;
+//        System.out.println("a=" + a);
+
+//        int s = 10;
+//        s += --s;
+//        System.out.println("s = " + s);
+    }
+}
