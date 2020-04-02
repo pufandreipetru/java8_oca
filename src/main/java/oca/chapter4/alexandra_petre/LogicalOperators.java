79df64ff79789204b27c37fab1dee659d1d62736
Alexandra Petre <v-alpetre@expediagroup.com>
2020-04-02 13:33:44 +0300

    chapter 4 - logical, relational ops

---
+++ src/main/java/oca/chapter4/alexandra_petre/LogicalOperators.java
@@ -0,0 +1,26 @@
+package oca.chapter4.alexandra_petre;
+
+/**
+ * |, &, ||, &&, ^, !
+ */
+public class LogicalOperators {
+    public static void main(String[] args) {
+        byte b1 = 8 & 4;
+        System.out.println(b1);
+
+        boolean a = false, b = false;
+//        boolean b2 = ( a == false ) && ( b = true);
+//        System.out.println(a + " - " + b2 + " - " + b);
+
+//        boolean b2 = ( a == false ) || ( b = true);
+//        System.out.println(a + " - " + b2 + " - " + b);
+
+//        boolean b2 = ( a == false ) | ( b = true);
+//        System.out.println(a + " - " + b2 + " - " + b);
+
+        // xor example
+//        System.out.println(( 3 > 5 ) ^ (20 < 0) );
+//        System.out.println(( 3 < 5 ) ^ (20 < 0) );
+//        System.out.println(( 3 > 5 ) ^ (20 > 0) );
+    }
+}
