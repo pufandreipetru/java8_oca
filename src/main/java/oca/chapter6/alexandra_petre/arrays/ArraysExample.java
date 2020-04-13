f0bfe9e119146bf58820bb12312c418c4f2a414f
Alexandra Petre <v-alpetre@expediagroup.com>
2020-04-13 10:45:23 +0300

    chapter 6 - arrays/arraylists

---
+++ src/main/java/oca/chapter6/alexandra_petre/arrays/ArraysExample.java
@@ -0,0 +1,51 @@
+package oca.chapter6.alexandra_petre.arrays;
+
+import java.util.Arrays;
+
+public class ArraysExample {
+    public static void main(String[] args) {
+        // Declaring
+//        int a1[];
+//        int[] a2;
+//        int []a3;
+
+//        String m1[][];
+//        String []m2[];
+//        String[] m3[][];
+
+//        int[5] a4;
+//        int a5[5];
+
+        // Constructing (what's on heap?)
+//        int array1 = new int[5];
+//        int array2 = new int[];
+//        int[][] array3 = new int[3][];
+
+        // Initializing
+//        int[] anArray = new int[3];
+//        anArray[2] = 5;
+//        anArray[3] = 6;
+//        System.out.println(anArray);
+//
+//        String[] anArray1 = new String[3];
+//        anArray1[2] = "AA";
+//        System.out.println(anArray1);
+//
+//        System.out.println(Arrays.toString(anArray));
+//        System.out.println(Arrays.toString(anArray1));
+
+//        int[] initArray1 = {1,2,3};
+//        System.out.println(Arrays.toString(initArray1));
+//        int[] initArray2 = new int[] {1,2,3};
+//        System.out.println(Arrays.toString(initArray2));
+
+//        int[][] biArray = new int[2][];
+//        biArray[0] = new int[2];
+//        biArray[1] = new int[3];
+//        System.out.println(Arrays.toString(biArray));
+//        System.out.println(Arrays.deepToString(biArray));
+
+//        int[] initArray3 = {1,'c',3};
+//        System.out.println(Arrays.toString(initArray3));
+    }
+}
